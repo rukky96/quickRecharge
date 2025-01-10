9e4a103473714598b340c0f8f36362e829250b29
Akproko Kelvin <akprokokelvin@gmail.com>
2025-01-10 10:40:51 +0100

    added html folder and home.html file

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.get('', (req, res) => {
-    res.send('This is to let you know that the server is running')
-})
-
-
+    res.sendFile(path.join(__dirname, 'public', '/html/home.html'))
+})``
 app.listen(port, () => {
     console.log("Server is Running");
 })
